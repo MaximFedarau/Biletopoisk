1b30fd20be1a77b917ac16f8fe3bdd7a2aedb081
maxim <fedaraumaxim@gmail.com>
2023-06-21 18:42:26 +0300

    fix: bottom footer 1) Update footer styles so that it always sits at the bottom of the page. 2) Update Card component styles & props.

--- src/components/Cards/Card/index.tsx
+++ src/components/Cards/Card/index.tsx
@@ -1,14 +1,16 @@
 import { FC, PropsWithChildren } from "react";
+import classNames from "classnames";
 
 import styles from "./styles.module.scss";
 
 interface Props extends PropsWithChildren {
   title: string;
+  className?: string;
 }
 
-export const Card: FC<Props> = ({ title, children }) => {
+export const Card: FC<Props> = ({ title, className, children }) => {
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.container, className)}>
       <p className={styles.container__title}>{title}</p>
       {children}
     </div>
