6c8d6b394bfd95712596874ef2af5b6d419d6929
maxim <fedaraumaxim@gmail.com>
2023-06-22 17:27:55 +0300

    feat: add Filters component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,63 @@
-import { NextPage } from "next";
+import { NextPage, Metadata } from "next";
 
-const Home: NextPage = () => {
+import { Filters } from "@/components/Filters";
+
+import styles from "./page.module.scss";
+
+export const metadata: Metadata = {
+  title: "Билетопоиск. Все фильмы планеты.",
+};
+
+const Page: NextPage = () => {
   return (
-    <main>
-      <h1>First!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+    <main className={styles.container}>
+      <Filters />
+      <section className={styles.container__cards}>
+        <h1>First!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
 
-      <h1>Hello!</h1>
-      <h1>Hello!</h1>
+        <h1>Hello!</h1>
+        <h1>Hello!</h1>
+      </section>
     </main>
   );
 };
 
-export default Home;
+export default Page;
---
+++ src/components/Filters/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import styles from "./styles.module.scss";
+
+export const Filters: FC = () => {
+  return (
+    <section className={styles.container}>
+      <section className={styles.container__content}>Filters</section>
+    </section>
+  );
+};
